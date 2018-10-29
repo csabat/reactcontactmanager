e7da30e1cd4d142c9db6467a2464779db2456c1d
csabat <toth.csaba1988@gmail.com>
2018-10-29 22:05:16 +0200

    Updates

---
+++ precache-manifest.7e3482a56e6f76ef8e2976d197cfac42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd5247a7f626e85626a",
+    "url": "/reactcontactmanager/static/js/runtime~main.dbd5247a.js"
+  },
+  {
+    "revision": "876c65bd3209464e99ae",
+    "url": "/reactcontactmanager/static/js/main.876c65bd.chunk.js"
+  },
+  {
+    "revision": "6f4708b992d8e49c72b2",
+    "url": "/reactcontactmanager/static/js/1.6f4708b9.chunk.js"
+  },
+  {
+    "revision": "876c65bd3209464e99ae",
+    "url": "/reactcontactmanager/static/css/main.e4be5b2f.chunk.css"
+  },
+  {
+    "revision": "6f4708b992d8e49c72b2",
+    "url": "/reactcontactmanager/static/css/1.226df324.chunk.css"
+  },
+  {
+    "revision": "e91a635e50b1d2dabb063c308bbfdcf0",
+    "url": "/reactcontactmanager/index.html"
+  }
+];
