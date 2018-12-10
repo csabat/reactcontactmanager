248af212dd10fbe9364a8109b648bf0b3616a5dd
csabat <toth.csaba1988@gmail.com>
2018-12-10 03:41:00 +0200

    small fix with the header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   return (
     <nav className = "navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
         <div className="container">
-            <Link href="/" className="navbar-brand">{props.branding}</Link>
+            <Link to="/" className="navbar-brand">{props.branding}</Link>
             <div>
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
